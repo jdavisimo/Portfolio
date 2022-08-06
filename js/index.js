2e60d035de09633cc31ef333999f88559f82c07e
jdavisimo <108847299+jdavisimo@users.noreply.github.com>
2022-08-06 13:22:34 -0500

    Google A

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 // Modal Image Gallery
+
 function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
@@ -8,25 +9,14 @@ function onClick(element) {
 }
 
 
-// .on event listener documentation - https://api.jquery.com/on/#on-events-selector-data-handler
+/* HOVER QUERY */
 
-// Step 1: Hide a div
-$('#phg1').on("click", function(){
-  $('.animate').animate( {: '250px'}, 1000 );
-});
+$( ".phg" ).hover(
+  function() {
+    $(.phg).addClass("hover");
+  }, function() {
+    $(.phg).removeClass( "hover" );
+  }
+);
 
-// Step 2: Change a color
-$('#button2').on("click", function(){
-  $('.cssChange').css("background-color","red");
-});
-
-// Step 3: Append content
-$('#button3').on("click", function(){
-  $('.appendMe').append("<div class='child'>I got appended! Nice!</div> ")
-});
-
-// Step 4: Animate a DIV
-$("button").click( function(){
-     $('.animate').animate( {left: '250px'}, 1000 );
-});
-
+$(".phg").off("mouseenter mouseleave");
